dbbaeb2754ddcc9fda0d3dcc940fdfc38d9898ef
Ian Ker-Seymer <i.kerseymer@gmail.com>
2014-11-12 08:39:39 -0700

    Make json tests green

---
+++ lib/rspec_api_helpers/json.rb
@@ -0,0 +1,11 @@
+module RspecApiHelpers
+  module Json
+    def response_body
+      @response_body ||= parse_json response.body
+    end
+
+    def parse_json(json)
+      ActiveSupport::HashWithIndifferentAccess.new(ActiveSupport::JSON.decode json)
+    end
+  end
+end
