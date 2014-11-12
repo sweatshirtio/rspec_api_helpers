581eee50f1033eecf7cbf0180afad47dbe958819
Ian Ker-Seymer <i.kerseymer@gmail.com>
2014-11-12 09:11:00 -0700

    Make headers spec green

---
+++ lib/rspec_api_helpers/headers.rb
@@ -0,0 +1,16 @@
+module RspecApiHelpers
+  module Headers
+    def headers_for(type)
+      _headers.send type
+    end
+
+    def _headers
+      headers = OpenStruct.new
+      headers.json = {
+        'CONTENT_TYPE' => 'application/json',
+        'ACCEPT' => 'application/json'
+      }
+      headers
+    end
+  end
+end
