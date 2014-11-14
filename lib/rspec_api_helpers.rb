1ef3482f71ac022b138b21c5edbc4941739750d4
Ian Ker-Seymer <i.kerseymer@gmail.com>
2014-11-13 22:36:17 -0700

    Include child submodules in RspecApiHelpers

--- lib/rspec_api_helpers.rb
+++ lib/rspec_api_helpers.rb
@@ -6,4 +6,12 @@ require 'rspec_api_helpers/strategies/json_strategy'
 require 'active_support/core_ext/object/json'
 require 'active_support/core_ext/hash'
 
+module RspecApiHelpers
+  def self.included(base)
+    constants.map(&method(:const_get)).each do |const|
+      base.include const if const.class == Module
+    end
+  end
+end
+
 FactoryGirl.register_strategy :json, RspecApiHelpers::Strategies::JsonStrategy
